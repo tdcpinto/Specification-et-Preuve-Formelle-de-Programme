412157751bb8b23f70e09a2ad83b1232f1c5c886
tdcpinto <theo.de-castro-pinto@u-bordeaux.fr>
2026-01-12 22:25:53 +0100

    TD2

--- TD1/src/ex1.c
+++ TD1/src/ex1.c
@@ -15,19 +15,8 @@ unsigned int mod2(unsigned int a, unsigned int b) {
     return a % (b + 1);
 }
 
-int mod3(unsigned int a, unsigned int b, unsigned int * result) {
-    if(b == UINT_MAX) {
-        return -1;
-    }
-    *result = a % (b + 1);
-    return 0;
-}
-
 int main() {
     mod(5, 2);
     mod2(5, 2);
-
-    unsigned int c;
-    mod3(4, 5, &c);
     return 0;
 }
---
+++ TD2/src/frama-c.sh
@@ -0,0 +1,6 @@
+OPAM_SWITCH_PREFIX=/net/npers/herbrete/.opam/default
+CAML_LD_LIBRARY_PATH=/net/npers/herbrete/.opam/default/lib/stublibs:/usr/lib64/ocaml/stublibs:/usr/lib64/ocaml
+OCAML_TOPLEVEL_PATH=/net/npers/herbrete/.opam/default/lib/toplevel
+MANPATH=$MANPATH:/net/npers/herbrete/.opam/default/man
+PATH=$PATH:/net/npers/herbrete/.opam/default/bin
+frama-c-gui "$@"
