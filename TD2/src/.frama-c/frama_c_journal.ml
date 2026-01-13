cfb7fdb7187db77b40bca5d6459a1f0610507866
tdcpinto <theo.de-castro-pinto@u-bordeaux.fr>
2026-01-13 15:24:25 +0100

    Fixed forall clause

---
+++ TD2/src/.frama-c/frama_c_journal.ml
@@ -0,0 +1,264 @@
+(* Frama-C journal generated at 14:22 the 13/01/2026 *)
+
+exception Unreachable
+exception Exception of string
+
+[@@@ warning "-26"]
+
+(* Run the user commands *)
+let run () =
+  Dynamic.Parameter.String.set ""
+    "/home/leop/Desktop/Work/Teaching/Specification-et-Preuve-Formelle-de-Programme-Private/public/TD2/src/example.c";
+  File.init_from_cmdline ();
+  Project.set_keep_current false;
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Report.classify_once";
+                    State.get "Property_status.Consolidated_status";
+                    State.get "PivotSourceState";
+                    State.get "Consolidation graph" ])
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Report.classify_once";
+                    State.get "Property_status.Consolidated_status";
+                    State.get "PivotSourceState";
+                    State.get "Consolidation graph" ])
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Report.classify_once";
+                    State.get "Property_status.Consolidated_status";
+                    State.get "PivotSourceState";
+                    State.get "Consolidation graph" ])
+    ();
+  Dynamic.Parameter.String.set "-wp-cache" "none";
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Report.classify_once";
+                    State.get "Property_status.Consolidated_status";
+                    State.get "PivotSourceState";
+                    State.get "Consolidation graph" ])
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Report.classify_once";
+                    State.get "Property_status.Consolidated_status";
+                    State.get "PivotSourceState";
+                    State.get "Consolidation graph" ])
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Report.classify_once";
+                    State.get "Property_status.Consolidated_status";
+                    State.get "PivotSourceState";
+                    State.get "Consolidation graph" ])
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Report.classify_once";
+                    State.get "Property_status.Consolidated_status";
+                    State.get "PivotSourceState";
+                    State.get "Consolidation graph" ])
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Report.classify_once";
+                    State.get "Property_status.Consolidated_status";
+                    State.get "PivotSourceState";
+                    State.get "Consolidation graph" ])
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Report.classify_once";
+                    State.get "Property_status.Consolidated_status";
+                    State.get "PivotSourceState";
+                    State.get "Consolidation graph" ])
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Report.classify_once";
+                    State.get "Property_status.Consolidated_status";
+                    State.get "PivotSourceState";
+                    State.get "Consolidation graph" ])
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Report.classify_once";
+                    State.get "Property_status.Consolidated_status";
+                    State.get "PivotSourceState";
+                    State.get "Consolidation graph" ])
+    ();
+  !Db.RteGen.annotate_kf (Globals.Functions.find_by_name "div");
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Report.classify_once";
+                    State.get "Property_status.Consolidated_status";
+                    State.get "PivotSourceState";
+                    State.get "Consolidation graph" ])
+    ();
+  Dynamic.Parameter.String.set "" "";
+  Dynamic.Parameter.String.set ""
+    "/home/leop/Desktop/Work/Teaching/Specification-et-Preuve-Formelle-de-Programme-Private/public/TD2/src/example.c";
+  File.init_from_cmdline ();
+  Project.set_keep_current false;
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Report.classify_once";
+                    State.get "Property_status.Consolidated_status";
+                    State.get "PivotSourceState";
+                    State.get "Consolidation graph" ])
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Report.classify_once";
+                    State.get "Property_status.Consolidated_status";
+                    State.get "PivotSourceState";
+                    State.get "Consolidation graph" ])
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Report.classify_once";
+                    State.get "Property_status.Consolidated_status";
+                    State.get "PivotSourceState";
+                    State.get "Consolidation graph" ])
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Report.classify_once";
+                    State.get "Property_status.Consolidated_status";
+                    State.get "PivotSourceState";
+                    State.get "Consolidation graph" ])
+    ();
+  !Db.RteGen.annotate_kf (Globals.Functions.find_by_name "div");
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Report.classify_once";
+                    State.get "Property_status.Consolidated_status";
+                    State.get "PivotSourceState";
+                    State.get "Consolidation graph" ])
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Report.classify_once";
+                    State.get "Property_status.Consolidated_status";
+                    State.get "PivotSourceState";
+                    State.get "Consolidation graph" ])
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Report.classify_once";
+                    State.get "Property_status.Consolidated_status";
+                    State.get "PivotSourceState";
+                    State.get "Consolidation graph" ])
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Report.classify_once";
+                    State.get "Property_status.Consolidated_status";
+                    State.get "PivotSourceState";
+                    State.get "Consolidation graph" ])
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Report.classify_once";
+                    State.get "Property_status.Consolidated_status";
+                    State.get "PivotSourceState";
+                    State.get "Consolidation graph" ])
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Report.classify_once";
+                    State.get "Property_status.Consolidated_status";
+                    State.get "PivotSourceState";
+                    State.get "Consolidation graph" ])
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Report.classify_once";
+                    State.get "Property_status.Consolidated_status";
+                    State.get "PivotSourceState";
+                    State.get "Consolidation graph" ])
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Report.classify_once";
+                    State.get "Property_status.Consolidated_status";
+                    State.get "PivotSourceState";
+                    State.get "Consolidation graph" ])
+    ();
+  ()
+
+(* Main *)
+let main () =
+  Journal.keep_file
+     (Datatype.Filepath.of_string
+     (".frama-c/frama_c_journal.ml"));
+  try run ()
+  with
+  | Unreachable -> Kernel.fatal "Journal reaches an assumed dead code" 
+  | Exception s -> Kernel.log "Journal re-raised the exception %S" s
+  | exn ->
+    Kernel.fatal
+      "Journal raised an unexpected exception: %s"
+      (Printexc.to_string exn)
+
+(* Registering *)
+let main : unit -> unit =
+  Dynamic.register
+    ~plugin:"Frama_c_journal.ml"
+    "main"
+    (Datatype.func Datatype.unit Datatype.unit)
+    ~journalize:false
+    main
+
+(* Hooking *)
+let () = Cmdline.run_after_loading_stage main; Cmdline.is_going_to_load ()
